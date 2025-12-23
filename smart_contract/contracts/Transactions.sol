9f2c3e864b29c7e3194e493c3e46ccd1759af45f
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-12-23 14:47:49 +0100

    Stable

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+// Create our contract and the name as to be the same with our file
 contract Transactions {
     // Variable that holds the number of our transactions
     uint256 transactionCount;
@@ -11,7 +12,7 @@ contract Transactions {
     // Create an event to emit when a transfer is made
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
-    // Create a struct to hold transaction details
+    // Create a struct to hold transaction details (transaction structure)
     struct TransferStruct {
         address sender;
         address receiver;
@@ -21,24 +22,24 @@ contract Transactions {
         string keyword;
     }
 
-    // Define an Array to hold all transactions
+    // Define an Array to hold all transactions: meaning an array of objects
     TransferStruct[] transactions;
 
-    // Function to add a transaction to the blockchain
+    // Function to add a transaction to the blockchain: memory message and memory receiver means these infos must be in the blockchain  
     function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
-        
+
         transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
-    // Function to get all transactions
+    // Function to get all transactions from the TransfertStruct[] array in the memory
     function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
 
-    // Function to get the number of transactions
+    // Function to get the number of transactions: returns transaction count which is a number
     function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,19 +1,22 @@
+// We need some "gas" -> small etherium in our wallet to make things happen.
+
 const main = async () => {
+  // transactionsFactory is the class that is going to generate the specific contracts.
   const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
   const transactionsContract = await transactionsFactory.deploy();
 
   await transactionsContract.deployed();
 
-  console.log("Transactions address: ", transactionsContract.address);
-};
+  console.log("Transactions deployed to address: ", transactionsContract.address);
+}; 
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0);
+    process.exit(0); // Simply means there is no error.
   } catch (error) {
     console.error(error);
-    process.exit(1);
+    process.exit(1); // Simply means there was an error.
   }
 };
 
